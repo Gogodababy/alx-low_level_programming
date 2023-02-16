f814d8adcc4ebff1dea7f97b32b3c807e96bce9d
jack <jackobleed@gmail.com>
2023-02-16 11:13:25 -0800

    final fix hopefully. task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /*
-* Entry function 
-* Return is 0 
+*Entry function 
+*Return is 0 
 *
 */
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
